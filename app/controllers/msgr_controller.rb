5b973156557959ba9b698fc764431009b2c6492b
53417 <seanimal@Live.com.au>
2019-10-25 20:31:41 +1100

    splash view template

--- app/controllers/msgr_controller.rb
+++ app/controllers/msgr_controller.rb
@@ -1,5 +1,16 @@
 class MsgrController < ApplicationController
-    def home
+    def login
         @logged_in_user = User.find_by :id => session[:user_id]
     end
+
+    # need to define user login in every single page
+    def splash
+        @logged_in_user = User.find_by :id => session[:user_id]
+    end
+
+    def chatrooms_list
+        @logged_in_user = User.find_by :id => session[:user_id]
+        @chatrooms = ChatroomUser.where(user_id: @logged_in_user)
+    end
+
 end
